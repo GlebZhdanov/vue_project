88ca70c3258953a2489bd75cd376a6feb2ae7449
Gleb Zhdanov <zhdanov-gleb@mail.ru>
2023-04-28 09:27:47 +0300

    add deploy

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const api = axios.create({
-	baseURL: "https://crudcrud.com/api/1ff5de14501d4a49824926b0201ea6ee",
+	baseURL: "https://crudcrud.com/api/37b3be17edc2474aaaf29ca7d0500fce",
 })
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require("@vue/cli-service")
 module.exports = {
 	transpileDependencies: true,
-	publicPath: process.env.NODE_ENV === "production"
-		? "/vue_project/"
-		: "/"
 }
